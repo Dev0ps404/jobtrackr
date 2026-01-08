49e89203296910acadb2b5b19de671c4ed5431ea
Devansh Agarwal <devanshaadhya@gmail.com>
2026-01-08 23:03:27 +0530

    fix: css and assets paths after vercel rewrite

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+// src/index.js
+import "./index.css"; // ✅ ye hona chahiye
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
